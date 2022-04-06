0e1183f6a7bf72260e5a274cc936b9416630dcc9
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-04-06 13:29:18 +0300

    login ui entries now work

--- src/ui/login_ui.py
+++ src/ui/login_ui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, ttk
+from tkinter import StringVar, Tk, ttk
 from services.shift_app_service import ShiftAppService
 
 
@@ -6,33 +6,34 @@ class Create_user_ui:
 
     def __init__(self,root):
         self._root = root
-        self._username_entry = None
-        self._password_entry = None
+        self._username_entry = StringVar()
+        self._password_entry = StringVar()
     
-    def handle_button_click(self):
-        username = self._username_entry.get()
-        password = self._password_entry.get()
-        ShiftAppService.create_user(username=username, password=password, role=None)
         
     def base(self):
         header = ttk.Label(master=self._root, text = "Create a user")
 
         username_header = ttk.Label(master=self._root, text= "Username")
 
-        username_entry = ttk.Entry(master=self._root)
+        self.username_entry = ttk.Entry(master=self._root, textvariable=self._username_entry)
 
         password_header = ttk.Label(master=self._root, text= "Password")
 
-        password_entry = ttk.Entry(master=self._root)
+        self.password_entry = ttk.Entry(master=self._root, textvariable=self._password_entry)
 
         create_button = ttk.Button(master=self._root, text="Create", command=self.handle_button_click)
 
         header.pack()
         username_header.pack()
-        username_entry.pack()
+        self.username_entry.pack()
         password_header.pack()
-        password_entry.pack()
+        self.password_entry.pack()
         create_button.pack()
+    
+    def handle_button_click(self):
+        username = self._username_entry
+        password = self._password_entry
+        ShiftAppService.create_user(username=username, password=password, role=None)
     
     
     def start():
