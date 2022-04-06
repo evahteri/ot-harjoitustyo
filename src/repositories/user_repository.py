75ed01fcefb09b779bd4056b1c7a3f18650f1593
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-04-06 15:23:23 +0300

    addedfind user function to find user from user database

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -4,6 +4,9 @@ from entities.user import User
 
 from db_connection import get_db_connection
 
+def return_user(row):
+    return User(row["username"], row["password"], row["role"])
+
 class UserRepository:
 
     def __init__(self):
@@ -17,10 +20,14 @@ class UserRepository:
 
     def create_user(self, user):
         cursor = self._connection.cursor()
-        print(user.username)
-        print("tuo oli käyttäjä")
         cursor.execute("INSERT INTO user_database(username,password,role) VALUES (?,?,?)", (user.username, user.password, user.role))
         self._connection.commit()
         cursor.close()
+    
+    def find_user(self, username):
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT * FROM user_database WHERE username = ?", username)
+        row = cursor.fetchone()
+        return return_user(row)
 
 
