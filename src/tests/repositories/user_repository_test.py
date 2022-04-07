b9eca8875bab1fe2e7f868cb3392d6514046cd81
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-04-07 13:06:47 +0300

    user creation test works again

--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -1,21 +1,18 @@
 import unittest
-import os.path
-
-from initialize_database import Create_databases
 from services.shift_app_service import ShiftAppService
 from repositories.user_repository import UserRepository
 from entities.user import User
+from initialize_database import CreateDatabases
 
 
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
+        CreateDatabases().create_user_database()
         self.user = User(username="Samuli",
-                         password="Gorilla12!", role="employee")
+                        password="Gorilla12!", 
+                        role="employee")
         ShiftAppService().create_user("Samuli", "Gorilla12!", "employee")
 
-    def test_user_database_file_exists(self):
-
-        self.assertEqual(os.path.exists("/ot-harjoitustyo/users.db"), True)
 
     def test_create_user_creates_a_user(self):
         user = UserRepository().find_user("Samuli")
