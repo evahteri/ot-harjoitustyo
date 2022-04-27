e7453542ce38d73b15bf1bb8b7b6ec666c4d0457
Eero Vahteri <eero.vahteri@helsinki.fi>
2022-04-27 14:33:20 +0300

    docstring added

--- src/entities/shift.py
+++ src/entities/shift.py
@@ -1,6 +1,15 @@
 class Shift:
 
     def __init__(self, date, time, location, employee):
+        """Constructor that creates a shift type object
+
+        Args:
+            date (string): date of the shift (e.g. 30.4.2022)
+            time (string): time of day (e.g. 6:30-12:00)
+            location (string): location of the shift
+            employee (string/None): assigned employee for the shift.
+
+        """
 
         self.date = date
         self.time = time
